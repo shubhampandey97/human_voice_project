893fd4e2c6b32bfe05c639fd16ee1c80bf5e40c9
Shubham Pandey <shubham17997@gmail.com>
2026-04-23 17:59:59 +0530

    Add hyperparameter tuning using GridSearchCV with MLflow tracking

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,5 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
 from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
@@ -9,17 +12,48 @@ def train_models(X_train, y_train):
     logger.info("Training models started")
 
     models = {}
+     # Random Forest Tuning
+    rf_params = {
+        "n_estimators": [100, 200],
+        "max_depth": [None, 10, 20],
+        "min_samples_split": [2, 5]
+    }
 
-    rf = RandomForestClassifier(n_estimators=200, random_state=42)
-    rf.fit(X_train, y_train)
+    rf = RandomForestClassifier(random_state=42)
+
+    rf_grid = GridSearchCV(
+        rf,
+        rf_params,
+        cv=3,
+        scoring="accuracy",
+        n_jobs=-1
+    )
+
+    rf_grid.fit(X_train, y_train)
     logger.info("Random Forest trained")
 
+    models["RandomForest"] = rf_grid
+
+    # SVM Tuning
+    svm_params = {
+        "C": [0.1, 1, 10],
+        "kernel": ["linear", "rbf"]
+    }
+
     svm = SVC(probability=True)
-    svm.fit(X_train, y_train)
+
+    svm_grid = GridSearchCV(
+        svm,
+        svm_params,
+        cv=3,
+        scoring="accuracy",
+        n_jobs=-1
+    )
+
+    svm_grid.fit(X_train, y_train)
     logger.info("SVM trained")
 
-    models["RandomForest"] = rf
-    models["SVM"] = svm
+    models["SVM"] = svm_grid
 
     logger.info("All models trained")
 
